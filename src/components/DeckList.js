0103831edd6c6b03aed4a962fd06670b44c629aa
Christopher Duggan <christopherduggan@gmail.com>
2019-04-21 14:17:57 -0400

    added decklist functionality, made plus and minus buttons increment/decrement card counts

--- src/components/DeckList.js
+++ src/components/DeckList.js
@@ -1,14 +1,52 @@
 import React from 'react'
 
 class DeckList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      deckArray: [
+        {name: `suq'ata lancer`, count: 1},
+        {name :`llanowar elves`, count: 2},
+        {name: `niv-mizzet, the firemind`, count: 4}
+      ]
+    }
+    this.incrementCard = this.incrementCard.bind(this)
+    this.decrementCard = this.decrementCard.bind(this)
+  }
+  incrementCard (e) {
+    let affectedCard = this.state.deckArray.findIndex((card) =>card.name === e.target.className)
+    if(this.state.deckArray[affectedCard].count < 4) {
+      this.setState(previousState => this.state.deckArray[affectedCard].count += 1)
+    }
+  }
+
+  decrementCard (e) {
+    console.log('this decreases the card count')
+
+    let affectedCard = this.state.deckArray.findIndex((card) =>card.name === e.target.className)
+    if(this.state.deckArray[affectedCard].count > 0) {
+      this.setState(previousState => this.state.deckArray[affectedCard].count -= 1)
+    }
+  }
+
+
+  // You're going to need spread (...) to add and remove cards from the array
+
   render() {
+    const makeList = this.state.deckArray.map(card => {
+      return (
+        <li key={card.name}>
+          {card.name}
+          x{card.count}
+          <span className={card.name} onClick={this.incrementCard}>+</span>
+          <span className={card.name} onClick={this.decrementCard}>-</span>
+        </li>
+      )
+    })
+
     return (
       <ul>
-        <li>A Card</li>
-        <li>A Card</li>
-        <li>A Card</li>
-        <li>A Card</li>
-        <li>A Card</li>
+        {makeList}
       </ul>
     )
   }
