33e1f201c346f715ba4ab5ac4533d8618e2cd4d9
Christopher Duggan <christopherduggan@gmail.com>
2019-04-23 16:50:35 -0400

    solved duplicates appearing in search

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,18 +1,31 @@
 import React from 'react'
 
 class Results extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state ={
+      displayList: []
+    }
+  }
   render() {
-    const renderResults = this.props.results.map(result => (
+    let displayList = []
 
-      <div key={result.id}>
-        <h3>{result.name}</h3>
-        <img src={result.imageUrl}/>
-        <button
-          onClick={() => this.props.addCardToList(result)}
-          name={result.name}
-          id={result.id}
-        >Add To Deck</button>
-      </div>))
+    const renderResults = this.props.results.map(result =>{
+      if(!displayList.includes(result.name)) {
+        displayList.push(result.name)
+        return(
+          <div key={result.id}>
+            <h3>{result.name}</h3>
+            <img src={result.imageUrl}/>
+            <button
+              onClick={() => this.props.addCardToList(result)}
+              name={result.name}
+              id={result.id}
+            >Add To Deck</button>
+          </div>
+        )
+      }
+    })
 
     return (
       <div>
